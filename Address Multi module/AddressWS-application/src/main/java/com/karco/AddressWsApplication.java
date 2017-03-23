676c73122bdd14258dc76dd40f8406340193a375
WafaBoussaid <wafa.boussaid@outlook.com>
2017-03-23 23:19:41 +0100

    Add files via upload

---
+++ Address Multi module/AddressWS-application/src/main/java/com/karco/AddressWsApplication.java
@@ -0,0 +1,12 @@
+package com.karco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressWsApplication.class, args);
+	}
+}
